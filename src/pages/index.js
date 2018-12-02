3d752ea91aa35b309475e5110bb5f991f7d74cbd
wduntak <wduntak@gmail.com>
2018-12-02 11:58:48 -0500

    Add User components with glamor css prop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,45 @@
 import React from "react"
 
-export default () => (
-  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-    <h1>Richard Hamming on Luck</h1>
-    <div>
-      <p>
-        From Richard Hamming’s classic and must-read talk, “
-        <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-          You and Your Research
-        </a>
-        ”.
-      </p>
-      <blockquote>
-        <p>
-          There is indeed an element of luck, and no, there isn’t. The prepared
-          mind sooner or later finds something important and does it. So yes, it
-          is luck.{" "}
-          <em>
-            The particular thing you do is luck, but that you do something is
-            not.
-          </em>
-        </p>
-      </blockquote>
+const Container = ({ children }) => (
+    <div css={{ margin: '3rem auto', maxWidth: 600 }}>{children}</div>
+)
+
+const User = props => (
+    <div
+      css={{
+        display: `flex`,
+        alignItems: `center`,
+        margin: `0 auto 12px auto`,
+        "&:last-child": { marginBottom: 0 },
+      }}
+    >
+  
+      <img
+        src={props.avatar}
+        css={{ flex: `0 0 96px`, width: 96, height: 96, margin: 0 }}
+        alt=""
+      />
+  
+      <div css={{ flex: 1, marginLeft: 18, padding: 12 }}>
+        <h2 css={{ margin: `0 0 12px 0`, padding: 0 }}>{props.username}</h2>
+        <p css={{ margin: 0 }}>{props.excerpt}</p>
+      </div>
     </div>
-    <p>Posted April 09, 2011</p>
-  </div>
+)
+
+export default () => (
+  <Container>
+    <h1>About Glamor</h1>
+    <p>Glamor is cool</p>
+    <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
 )
